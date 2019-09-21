6263e370d0f3ca9d1f45639576db72718b12909c
nitinakash1989 <nitin.akash1989@gmail.com>
2019-09-21 15:28:52 -0300

    Added program description.

--- EvenOddFinder.java
+++ EvenOddFinder.java
@@ -1,5 +1,9 @@
 public class EvenOddFinder{
 
+  /*
+  Write a program to accept a number and display whether a number is odd
+  or even using the ternary operator.
+  */
   public static boolean isEven(int number){
      return number%2==0?true:false;
   }
